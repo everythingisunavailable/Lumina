f58cd1b62e5b8632989a0e57172af8ba848b409c
frenki <frenkiqose@gmail.com>
2025-03-30 23:40:16 +0200

    - fixes some navbar spacing and scrolling when viewing a movie

--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal} from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, signal} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../models/movie.model';
 import { GetMoviesService } from '../services/get-movies.service';
@@ -10,11 +10,15 @@ import { LongSliderComponent } from '../components/long-slider/long-slider.compo
   templateUrl: './movie-info.component.html',
   styleUrl: './movie-info.component.scss'
 })
-export class MovieInfoComponent implements OnInit{
+export class MovieInfoComponent implements OnInit, AfterViewInit{
   movie = signal<Movie | null>(null);
   
   private movieService = inject(GetMoviesService);
   constructor(private route: ActivatedRoute){}
+
+  ngAfterViewInit(): void {
+    window.scrollTo({top: 0});
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( params => {
